cde8e99314cca82e9e6b2e4273978e1d6b8cc476
Alikanagreeshma <alikanagreeshma@gmail.com>
2026-03-29 16:02:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from explanation import generate_explanation
 
 app = Flask(__name__)
 CORS(app)
-# Load dataset
+
 with open("skills.json") as f:
     skills_data = json.load(f)
 
@@ -18,17 +18,17 @@ def analyze():
     data = request.get_json()
     skill_name = data.get("skill").lower()
 
-    # Check if skill exists
+    
     if skill_name not in skills_data:
         return jsonify({"error": "Skill not found"})
 
     skill = skills_data[skill_name]
 
-    # 🔥 Use AI explanation engine
+    
     result = generate_explanation(skill_name, skill)
 
     return jsonify(result)
 
-# Run server
+
 if __name__ == '__main__':
     app.run(debug=True)
